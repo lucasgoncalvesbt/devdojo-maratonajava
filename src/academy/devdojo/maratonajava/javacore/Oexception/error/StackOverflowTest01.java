b5fea83f647b9db3117d497b302e09d00540b315
Lucas Gonçalves <front.end.lb@gmail.com>
2021-10-29 12:21:14 -0300

    aulas 95-130 finalizadas

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/error/StackOverflowTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Oexception.error;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void recursividade(){
+        recursividade();
+    }
+}
